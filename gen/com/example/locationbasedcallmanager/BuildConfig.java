579a5066cd30e6d71c0a54212fda800f7db33fed
nipun123 <nipun.udara@yahoo.com>
2013-08-06 06:54:11 +0530

    Adding new project to github

---
+++ gen/com/example/locationbasedcallmanager/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.locationbasedcallmanager;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
